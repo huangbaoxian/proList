6458695edb7910db155acd77a72f0d717cb7cde3
huangbx <huangbx@example.com>
2019-07-05 16:17:02 +0800

    新增代码

--- proList/page/webVc/HBXWebViewController.m
+++ proList/page/webVc/HBXWebViewController.m
@@ -13,7 +13,7 @@
 
 @interface HBXWebViewController ()<WKUIDelegate, WKNavigationDelegate>
 
-@property (nonatomic, strong) WKWebView *webView;
+@property (nonatomic, strong) UIWebView *webView;
 @property (nonatomic, strong) UIView *toolBar;
     
 @end
@@ -24,10 +24,10 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     if (!_webView) {
-        _webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, PHONE_NAVIGATIONBAR_HEIGHT, self.view.width, self.view.height - PHONE_NAVIGATIONBAR_HEIGHT - kTabbarSafeBottomMargin - KBottomHeight)];
+        _webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, PHONE_NAVIGATIONBAR_HEIGHT, self.view.width, self.view.height - PHONE_NAVIGATIONBAR_HEIGHT - kTabbarSafeBottomMargin - KBottomHeight)];
         [self.view addSubview:_webView];
-        _webView.UIDelegate = self;
-        _webView.navigationDelegate = self;
+//        _webView.UIDelegate = self;
+//        _webView.navigationDelegate = self;
         _toolBar = [[UIView alloc] initWithFrame:CGRectMake(0, self.view.height - KBottomHeight- kTabbarSafeBottomMargin, self.view.width, KBottomHeight)];
         [self.view addSubview:_toolBar];
         [self creatItems];
@@ -52,7 +52,7 @@
     CGFloat space = 15;
     
     self.toolBar.userInteractionEnabled = YES;
-    NSArray *array = @[@"icon_date_left_nol",@"icon_date_right_nol",@"ic_loading_rotate",@"home", @"退出"];
+    NSArray *array = @[@"后退",@"前进",@"刷新",@"首页", @"退出"];
     for (int i = 0 ; i < array.count; i++) {
         UIButton *btn = [UIButton buttonWithType: UIButtonTypeCustom];
         
@@ -62,17 +62,8 @@
         btn.tag = i + 1000;
         [self.toolBar addSubview:btn];
         btn.frame = CGRectMake(15 + i *(btnWith + space), 0, btnWith, btnHeight);
+        [btn setTitle:array[i] forState:UIControlStateNormal];
         
-        if (i == array.count - 1) {
-            [btn setTitle:@"退出" forState:UIControlStateNormal];
-        }else {
-            UIImageView *imageView = [[UIImageView alloc] init];
-            imageView.frame = CGRectMake((btnWith - 30)/2, 7, 30, 30);
-            [btn addSubview:imageView];
-            imageView.image = [UIImage imageNamed:array[i]];
-        }
-        
-       
     }
 }
     
@@ -97,9 +88,11 @@
         case 3:
                 {
                     [SVProgressHUD showWithStatus:@"加载中..."];
-                    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"https://cpbang.app-updated.com/wap/iosapp/iosxiazai/index.html"]];
+                    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                        [SVProgressHUD dismiss];
+                    });
+                    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:APPCONTEXT.mainUrl]];
                     [self.webView loadRequest:request];
-                    
                 }
         break;
         case 4: {
@@ -114,26 +107,15 @@
     
 #pragma mark - WKNavigationDelegate
     // 页面开始加载时调用
-- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation{
-    
-}
-    // 当内容开始返回时调用
-- (void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation{
-    
-}
-    // 页面加载完成之后调用
-- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation{
-    NSLog(@"succ");
-    [SVProgressHUD dismiss];
-}
-    // 页面加载失败时调用
-- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation{
-    NSLog(@"failed");
-}
-    // 接收到服务器跳转请求之后调用
-- (void)webView:(WKWebView *)webView didReceiveServerRedirectForProvisionalNavigation:(WKNavigation *)navigation{
-    
-}
+   // 页面加载完成之后调用
+//- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation{
+//    NSLog(@"succ");
+//    [SVProgressHUD dismiss];
+//}
+//    // 页面加载失败时调用
+//- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation{
+//    NSLog(@"failed");
+//}
 
 /*
 #pragma mark - Navigation
